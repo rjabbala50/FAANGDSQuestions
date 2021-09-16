fca0d1e68c2346afe099a3aab01c90da087ffd90
Rajamohan Jabbala <rjabbala@gmail.com>
2021-09-17 05:24:23 +0530

    Find min max

--- Arrays/FindMinMax.java
+++ Arrays/FindMinMax.java
@@ -1,4 +1,25 @@
+/*
+ * Find min and max from Array
+ */
 
+class GetMinimum  {
+	protected int getMin(int[] input, int size) {
+		int min=65535;
+	
+		for (int val:input) 
+			if( val<min )
+				min = val ;
+		return min ;
+   	}	
+}
+
+public class FindMinMax {
+	public static void main(String[] args) {
+		int[] input = {2,3,9,4};
+		GetMinimum gm = new GetMinimum() ;
+		System.out.println("Min Elt: "+gm.getMin(input,4)) ;
+	}
+}
 
 
 
